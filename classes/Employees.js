20c28f1c80d32826e209b9234c32d02d664ae5ef
twygw <stanley.dix@sky.uk>
2023-10-25 12:57:07 +0100

    init commit finished employee

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,31 @@
 class Employees {
-
-
-
-    
+  static Employees = [];
+  #salary;
+  #isHired;
+  constructor(name, title, salary) {
+    this.name = name;
+    this.title = title;
+    this.#salary = salary;
+    this.#isHired = true;
+  }
+  getSalary() {
+    return this.#salary;
+  }
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+  getStatus() {
+    return this.#isHired;
+  }
+  setStatus(command) {
+    if (command == "hire") {
+      this.#isHired = true;
+    } else if (command == "fire") {
+      this.#isHired = false;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
